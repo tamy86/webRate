5562b1190ff5cf712145dc72752e0b8b21afedbd
tamy86 <34414031+tamy86@users.noreply.github.com>
2020-01-02 14:27:38 -0800

    commit2

---
+++ webrate/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CenteredGrid from './Header';
+import 'c:/Users/tahmoores19/Documents/GitHub/my-app1/webRate/webrate/src/components/App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <CenteredGrid/>
+    </div>
+  );
+}
+
+export default App;
